9f17e43beac4a828accabcaab00191634db23165
Elif <eliful@gmail.com>
2022-10-28 15:20:41 +0200

    commit after merge pascaline 2

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
+    <div className="footer">
       {" "}
       <div className="logoFooter">
         <img src="/logos/logo-wild.png" alt="logoWild" />
