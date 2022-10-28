153472cfae0cd9e2089bf3a0efd9cda2a614a8ec
Elif <eliful@gmail.com>
2022-10-28 15:02:35 +0200

    commit before pull variables scss

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      {" "}
+      <div className="logoFooter">
+        <img src="/logos/logo-wild.png" alt="logoWild" />
+      </div>
+      <div className="creditsFooter">
+        <h2>© CRÉDITS</h2>
+        <a href="https://fontawesome.com/icons">
+          https://fontawesome.com/icons
+        </a>
+        <a href="https://www.pexels.com">https://www.pexels.com</a>
+      </div>
+      <div className="teamFooter">
+        <h2>TEAM’S LINKEDINS</h2>
+        <p>
+          Yossef Kasabi, Cyril Leclercq, Pascaline Pierre, Dinh Huy Trinh, Elif
+          Uluengin
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
