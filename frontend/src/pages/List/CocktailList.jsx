4d92597bd0ec69d0b86b2a6766e62b109aed2288
Cylecq <leclercq.cyril06@gmail.com>
2022-11-02 12:04:26 +0100

    Modif card + list Desktop done

--- frontend/src/pages/List/CocktailList.jsx
+++ frontend/src/pages/List/CocktailList.jsx
@@ -3,8 +3,14 @@ import Card from "../../components/Card";
 function CocktailList() {
   return (
     <div className="cocktailList">
-      <Card />
-      <Card />
+      <h2 className="cocktailListTitle">Nos Cocktails</h2>
+      <div className="CocktailListCards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
