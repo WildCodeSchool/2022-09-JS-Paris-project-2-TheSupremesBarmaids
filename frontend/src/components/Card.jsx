034272617193529d3d644691dd51edf02ff79b90
Cylecq <leclercq.cyril06@gmail.com>
2022-11-04 13:31:43 +0100

    Suite

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,20 +1,38 @@
-function Card() {
+import { PropTypes } from "prop-types";
+
+function Card({ posts, loading }) {
+  if (loading) {
+    return <h2>loading</h2>;
+  }
+
   return (
-    <div className="cardContainer">
-      <div className="card">
-        <img
-          className="cardImage"
-          src="/images/imgCocktail.jpg"
-          alt="cocktail"
-        />
-        <div className="notFavorite" />
-        <div className="cardText">
-          <h3 className="cardTitle">Cocktail Name</h3>
-          <div>*Note*</div>
+    <div>
+      {posts.map((post) => (
+        <div className="cardContainer" key={post.idDrink}>
+          <div className="card">
+            <img
+              className="cardImage"
+              src={post.strDrinkThumb}
+              alt={post.strDrink}
+            />
+            <div className="notFavorite" />
+            <div className="cardText">
+              <h3 className="cardTitle">{post.strDrink}</h3>
+              <div>*Note*</div>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
+
+Card.propTypes = {
+  posts: PropTypes.arrayOf.isRequired,
+};
+
+Card.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
 
 export default Card;
--- frontend/src/pages/List/CocktailList.jsx
+++ frontend/src/pages/List/CocktailList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../../components/Card";
