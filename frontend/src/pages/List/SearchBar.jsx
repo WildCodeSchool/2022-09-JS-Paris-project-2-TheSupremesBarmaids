8ce5d48e204473529677b4de500b086ef08cbf1f
Cylecq <leclercq.cyril06@gmail.com>
2022-11-10 12:06:37 +0100

    SearchBar done

--- frontend/src/pages/List/SearchBar.jsx
+++ frontend/src/pages/List/SearchBar.jsx
@@ -1,27 +1,36 @@
-/* eslint-disable import/no-unresolved */
 /* eslint-disable react/prop-types */
-// eslint-disable-next-line import/no-absolute-path
-import SearchIcon from "/public/icones/search.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function SearchBar({ callApi }) {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    callApi("search.php?", "s=", searchTerm);
+  };
+
+  const isSearchBarEmpty = searchTerm.length === 0;
+
+  useEffect(() => {
+    if (isSearchBarEmpty) callApi("filter.php?", "i=", "Gin");
+  }, [isSearchBarEmpty]); // Reset list if the search bar is empty
+
   return (
-    <div className="search">
+    <form className="search-box" role="search">
+      <label htmlFor="search" className="srOnly">
+        Search Cocktails
+      </label>
       <input
-        className="js-search"
+        className="search-input"
+        type="search"
         placeholder="Search Cocktails"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <img
-        src={SearchIcon}
-        alt="search"
-        onClick={() => callApi("search.php?", "s=", searchTerm)}
-        aria-hidden="true"
-      />
-    </div>
+      <button className="search-button" type="submit" onClick={handleSubmit}>
+        <img src="/public/icones/searchColorLeather.svg" alt="searchIcon" />
+      </button>
+    </form>
   );
 }
 
