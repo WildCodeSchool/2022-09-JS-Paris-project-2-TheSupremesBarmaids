c75ee4b7cdbf236a802c54b6f9cf02b92a120085
Cylecq <leclercq.cyril06@gmail.com>
2022-11-15 15:11:17 +0100

    Dropdown component created

--- frontend/src/pages/List/Filter.jsx
+++ frontend/src/pages/List/Filter.jsx
@@ -1,46 +1,17 @@
-import { useState } from "react";
-import FilterAlcohol from "./FilterAlcohol";
-import FilterCategory from "./FilterCategory";
-import FilterIngredient from "./FilterIngredient";
+import DropdownFilterAlcohol from "./DropdownFilterAlcohol";
+import DropdownFilterCategory from "./DropdownFilterCategory";
+import DropdownFilterIngredient from "./DropdownFilterIngredient";
 import ResetButton from "./ResetButton";
 
 function Filter() {
-  const [isAlcoholicOpened, setIsAlcoholicOpened] = useState(false);
-  const [isCategoryOpened, setIsCategoryOpened] = useState(false);
-  const [isIngredientsOpened, setIsIngredientsOpened] = useState(false);
-
-  const handleOpenAlcoholic = () => setIsAlcoholicOpened(!isAlcoholicOpened);
-  const handleOpenCategory = () => setIsCategoryOpened(!isCategoryOpened);
-  const handleOpenIngredients = () =>
-    setIsIngredientsOpened(!isIngredientsOpened);
-
   return (
     <div className="filter-btn">
-      {/* ALCOHOLIC FILTER BUTTON */}
-      <div className="dropdown-filter">
-        <button type="button" onClick={handleOpenAlcoholic}>
-          Alcoholic
-        </button>
-        {isAlcoholicOpened ? <FilterAlcohol /> : null}
-      </div>
+      <DropdownFilterAlcohol />
 
-      {/* CACTEGORIES FILTER BUTTON */}
-      <div className="dropdown-filter">
-        <button type="button" onClick={handleOpenCategory}>
-          Categories
-        </button>
-        {isCategoryOpened ? <FilterCategory /> : null}
-      </div>
+      <DropdownFilterCategory />
 
-      {/* INGREDIENTS FILTER BUTTON */}
-      <div className="dropdown-filter">
-        <button type="button" onClick={handleOpenIngredients}>
-          Ingredients
-        </button>
-        {isIngredientsOpened ? <FilterIngredient /> : null}
-      </div>
+      <DropdownFilterIngredient />
 
-      {/* RESET BUTTON */}
       <ResetButton />
     </div>
   );
