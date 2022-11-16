f2bed4ba9f7e0ae2a222ff08bdda0317eae0930f
yossef199 <kassabiyossef@gmail.Com>
2022-11-16 11:24:59 +0100

    toggle-actionblock done

--- frontend/src/pages/List/ActionBlock.jsx
+++ frontend/src/pages/List/ActionBlock.jsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+import { ToggleContext } from "../../services/Context";
 import SearchBar from "./SearchBar";
 import Filter from "./Filter";
 
 function ActionBlock() {
+  const { isActionBlockOpened } = useContext(ToggleContext);
+
   return (
-    <div className="action-block">
+    <div
+      className={
+        isActionBlockOpened ? "actionBlockOpened action-block" : "action-block"
+      }
+    >
       <SearchBar />
       <Filter />
     </div>
