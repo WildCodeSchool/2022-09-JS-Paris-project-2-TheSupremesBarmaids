6a064610b0d558d839f62693004d800c604b2e37
Cylecq <leclercq.cyril06@gmail.com>
2022-11-17 11:58:23 +0100

    Randoom Cocktail done

--- frontend/src/components/RandomLink.jsx
+++ frontend/src/components/RandomLink.jsx
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import fetchRandomApi from "../utils/fetchRandomApi";
 
 function RandomLink() {
+  const [random, setRandom] = useState();
+
+  useEffect(() => {
+    fetchRandomApi().then((resPosts) => {
+      setRandom(resPosts.strDrink);
+    });
+  }, []);
+
   return (
-    <Link to="/recipe/margarita" className="buttonType">
-      <span className="buttonType__text">On choisit pour vous ?</span>
+    <Link to={`/recipe/${random}`} className="buttonType">
+      <span className="buttonType__text">Pick one for me !</span>
       <span className="buttonType__icon" />
     </Link>
   );
