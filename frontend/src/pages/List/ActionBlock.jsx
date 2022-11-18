bbd53b537ed50ed5891d90d6f4d94d06b8c0f614
Cylecq <leclercq.cyril06@gmail.com>
2022-11-18 12:18:33 +0100

    show only searchbar + toggle filters

--- frontend/src/pages/List/ActionBlock.jsx
+++ frontend/src/pages/List/ActionBlock.jsx
@@ -1,17 +1,9 @@
-import { useContext } from "react";
-import { ToggleContext } from "../../services/Context";
 import SearchBar from "./SearchBar";
 import Filter from "./Filter";
 
 function ActionBlock() {
-  const { isActionBlockOpened } = useContext(ToggleContext);
-
   return (
-    <div
-      className={
-        isActionBlockOpened ? "actionBlockOpened action-block" : "action-block"
-      }
-    >
+    <div className="action-block">
       <SearchBar />
       <Filter />
     </div>
