92e3b6117e704abbf5bdca37fa88056dbc7ae9e9
paskaphot <ppierre.network@gmail.com>
2022-11-22 17:22:52 +0100

    delete unused icons + favorite + stars note

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,16 +1,7 @@
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-
-import Note from "./Note";
-
 function Card({ id, imgSrc, imgAlt, name }) {
   return (
     <a href={`/recipe/${name}`} className="card" id={id}>
       <img className="cardImage" src={imgSrc} alt={imgAlt} />
-      <div className="favoriteCocktail">
-        <AiFillHeart className="isFavorite" />
-        <AiOutlineHeart className="notFavorite" />
-      </div>
-      <Note />
       <p className="cardTitle">{name}</p>
     </a>
   );
---
+++ frontend/src/components/Stars.jsx
@@ -0,0 +1,13 @@
+import { AiFillStar } from "react-icons/ai";
+
+function Stars() {
+  return (
+    <div className="decoStars">
+      <AiFillStar className="star" />
+      <AiFillStar className="star" />
+      <AiFillStar className="star" />
+    </div>
+  );
+}
+
+export default Stars;
