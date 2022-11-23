179baefd14048457682ae496cf929b661a10a869
Huy Trinh <laser2u02@gmail.com>
2022-11-23 11:26:50 +0100

    clean presentation

--- frontend/src/pages/Details/Commentaire.jsx
+++ frontend/src/pages/Details/Commentaire.jsx
@@ -17,16 +17,6 @@ function Commentaire() {
           </div>
         </ContextProvider>
       </div>
-      <div className="reviews">
-        <h3 className="reviewTitle linedTitle">
-          <span className="linedTitle__text">Leave us your opinion</span>
-        </h3>
-        <div className="reviewNote">*Note*</div>
-        <div className="reviewBox">
-          <input type="textbox" id="review" name="review" maxLength={75} />
-          <button type="submit">Submit</button>
-        </div>
-      </div>
     </div>
   );
 }
